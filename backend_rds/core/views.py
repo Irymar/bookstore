8cb34ffe679af3de6ada4b82276b4504165cdfbe
Irymar <irymar97@gmail.com>
2025-06-22 12:54:32 +0300

    test message

--- backend_rds/core/views.py
+++ backend_rds/core/views.py
@@ -10,7 +10,7 @@ def test_connection(request):
             cursor.execute("SELECT 1")
             row = cursor.fetchone()
             if row and row[0] == 1:
-                return JsonResponse({'message': 'Connection to Backend RDS is successful!'})
+                return JsonResponse({'message': 'Hello!'})
             else:
                 logger.error("Database query returned unexpected result")
                 return JsonResponse({'error': 'Database query failed'}, status=500)
